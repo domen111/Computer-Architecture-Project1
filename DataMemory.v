e9d6a0faed1f12cc49ba07ce5f2000ddce7f4de9
codingbeans <asd768999@gmail.com>
2017-12-06 00:05:13 +0800

    Modify DataMemory.v

--- DataMemory.v
+++ DataMemory.v
@@ -1,22 +1,22 @@
 module dm
 (
-    input wire          clk,
-    input wire	[6:0]   addr,
-    input wire          MemRead,
-    input wire          MemWrite,
-    input wire 	[31:0]  wdata,
-    output wire	[31:0]  rdata
+    input wire          clk_i,
+    input wire	[6:0]   addr_i,
+    input wire          memRead_i,
+    input wire          memWrite_i,
+    input wire 	[31:0]  wData_i,
+    output wire	[31:0]  rData_o
 );
 
 // Memories
 reg [31:0] mem [0:127];
 
-always @(posedge clk) begin
-	if( MemWrite ) begin
-        mem[addr] <= wdata;
+always @(posedge clk_i) begin
+	if( memWrite_i ) begin
+        mem[addr_i] <= wdata_i;
 	end
 end
 
-assign rdata = MemRead ? mem[addr][31:0]: wdata;
+assign rdata_o = memRead_i ? mem[addr_i][31:0]: wdata_i;
 
 endmodule
