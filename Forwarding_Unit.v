6ca96041d5d69c5c2103b1a67df4b34f79d7b6a6
codingbeans <asd768999@gmail.com>
2017-12-10 21:20:14 +0800

    Modify Forwarding_Unit.v for naming consistency

--- Forwarding_Unit.v
+++ Forwarding_Unit.v
@@ -2,39 +2,39 @@
 
 module Forwarding_Unit
 (
-    input [4:0] DecRsAddr_i,
-    input [4:0] DecRtAddr_i,
-    input ExMemRegWrite_i,
-    input [4:0] ExMemRdAddr_i,
-    input MemWbRegWrite_i,
-    input [4:0] MemWbRdAddr_i,
-    output reg [1:0] DecRsOverride_o,
-    output reg [1:0] DecRtOverride_o
+    input [4:0] ID_EX_RsAddr_i,
+    input [4:0] ID_EX_RtAddr_i,
+    input EX_MEM_RegWrite_i,
+    input [4:0] EX_MEM_RdAddr_i,
+    input MEM_WB_RegWrite_i,
+    input [4:0] MEM_WB_RdAddr_i,
+    output reg [1:0] EX_RsOverride_o,
+    output reg [1:0] EX_RtOverride_o
 );
 
 always @(*)
 begin
     
     // default
-    DecRsOverride_o = 2'b00;
-    DecRtOverride_o = 2'b00;
+    EX_RsOverride_o = 2'b00;
+    EX_RtOverride_o = 2'b00;
     
-    if(ExMemRegWrite_i && (ExMemRdAddr_i != `ZeroReg))
+    if(EX_MEM_RegWrite_i && (EX_MEM_RdAddr_i != `ZeroReg))
     begin
-        if(ExMemRdAddr_i == DecRsAddr_i)
-            DecRsOverride_o <= 2'b10;
-        if(ExMemRdAddr_i == DecRtAddr_i)
-            DecRtOverride_o <= 2'b10;
+        if(EX_MEM_RdAddr_i == ID_EX_RsAddr_i)
+            EX_RsOverride_o <= 2'b10;
+        if(EX_MEM_RdAddr_i == ID_EX_RtAddr_i)
+            EX_RtOverride_o <= 2'b10;
     end
 
-    if(MemWbRegWrite_i && (MemWbRdAddr_i != `ZeroReg))
+    if(MEM_WB_RegWrite_i && (EX_MEM_RdAddr_i != `ZeroReg))
     begin
-        if(ExMemRdAddr_i != DecRsAddr_i &&
-          (MemWbRdAddr_i == DecRsAddr_i))
-            DecRsOverride_o <= 2'b01;
-        if(ExMemRdAddr_i != DecRtAddr_i &&
-          (MemWbRdAddr_i == DecRtAddr_i))
-            DecRtOverride_o <= 2'b01;
+        if(EX_MEM_RdAddr_i != ID_EX_RsAddr_i &&
+          (MEM_WB_RdAddr_i == ID_EX_RsAddr_i))
+            EX_RsOverride_o <= 2'b01;
+        if(EX_MEM_RdAddr_i != ID_EX_RtAddr_i &&
+          (MEM_WB_RdAddr_i == ID_EX_RtAddr_i))
+            EX_RtOverride_o <= 2'b01;
     end
 end
 
