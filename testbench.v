1d0f7a469de4479c1fc0aaa659e500d43a6f3aef
domen111 <domen111@gmail.com>
2017-12-12 21:46:40 +0800

    Update testbench

--- testbench.v
+++ testbench.v
@@ -57,12 +57,12 @@ initial begin
 end
   
 always@(posedge Clk) begin
-    if(counter == 10)    // stop after 30 cycles
+    if(counter == 30)    // stop after 30 cycles
         $stop;
 
     // put in your own signal to count stall and flush
-    // if(CPU.HazzardDetection.mux8_o == 1 && CPU.Control.Jump_o == 0 && CPU.Control.Branch_o == 0)stall = stall + 1;
-    // if(CPU.HazzardDetection.Flush_o == 1)flush = flush + 1;  
+    if(CPU.Hazard_Detection_Unit.stall_o == 1 && CPU.Control.Jump_o == 0 && CPU.Control.Branch_o == 0)stall = stall + 1;
+    if(CPU.IF_ID_Flush.Flush_o == 1)flush = flush + 1;  
 
     // print PC
     $fdisplay(outfile, "cycle = %d, Start = %d, Stall = %d, Flush = %d\nPC = %d", counter, Start, stall, flush, CPU.PC.pc_o);
