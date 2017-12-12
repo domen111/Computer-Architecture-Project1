2d5de22c383297f0e815c9a0b33548a32cf00d56
domen111 <domen111@gmail.com>
2017-12-12 15:31:07 +0800

    Change output of Hazard_Detection_Unit and Forwarding_Unit to wire

--- Forwarding_Unit.v
+++ Forwarding_Unit.v
@@ -8,34 +8,40 @@ module Forwarding_Unit
     input [4:0] EX_MEM_RdAddr_i,
     input MEM_WB_RegWrite_i,
     input [4:0] MEM_WB_RdAddr_i,
-    output reg [1:0] EX_RsOverride_o,
-    output reg [1:0] EX_RtOverride_o
+    output [1:0] EX_RsOverride_o,
+    output [1:0] EX_RtOverride_o
 );
+
+reg [1:0] EX_RsOverride_o_temp;
+reg [1:0] EX_RtOverride_o_temp;
 
 always @(*)
 begin
     
     // default
-    EX_RsOverride_o = 2'b00;
-    EX_RtOverride_o = 2'b00;
+    EX_RsOverride_o_temp = 2'b00;
+    EX_RtOverride_o_temp = 2'b00;
     
     if(EX_MEM_RegWrite_i && (EX_MEM_RdAddr_i != `ZeroReg))
     begin
         if(EX_MEM_RdAddr_i == ID_EX_RsAddr_i)
-            EX_RsOverride_o <= 2'b10;
+            EX_RsOverride_o_temp <= 2'b10;
         if(EX_MEM_RdAddr_i == ID_EX_RtAddr_i)
-            EX_RtOverride_o <= 2'b10;
+            EX_RtOverride_o_temp <= 2'b10;
     end
 
     if(MEM_WB_RegWrite_i && (EX_MEM_RdAddr_i != `ZeroReg))
     begin
         if(EX_MEM_RdAddr_i != ID_EX_RsAddr_i &&
           (MEM_WB_RdAddr_i == ID_EX_RsAddr_i))
-            EX_RsOverride_o <= 2'b01;
+            EX_RsOverride_o_temp <= 2'b01;
         if(EX_MEM_RdAddr_i != ID_EX_RtAddr_i &&
           (MEM_WB_RdAddr_i == ID_EX_RtAddr_i))
-            EX_RtOverride_o <= 2'b01;
+            EX_RtOverride_o_temp <= 2'b01;
     end
 end
+
+assign EX_RsOverride_o = EX_RsOverride_o_temp;
+assign EX_RtOverride_o = EX_RtOverride_o_temp;
 
 endmodule
--- Hazard_Detection_Unit.v
+++ Hazard_Detection_Unit.v
@@ -4,26 +4,34 @@ module Hazard_Detection_Unit
     input [4:0] IF_ID_RsAddr_i,
     input [4:0] IF_ID_RtAddr_i,
     input [4:0] ID_EX_RtAddr_i,
-    output reg PC_Stall_o,
-    output reg IF_ID_Stall_o,
-    output reg stall_o
+    // output reg PC_Stall_o,
+    // output reg IF_ID_Stall_o,
+    // output reg stall_o
+    output PC_Stall_o,
+    output IF_ID_Stall_o,
+    output stall_o
 );
 
-always@(*)
-begin
+
+assign {PC_Stall_o, IF_ID_Stall_o, stall_o} =
+        (ID_EX_MemRead_i && ((ID_EX_RtAddr_i == IF_ID_RsAddr_i) || (ID_EX_RtAddr_i == IF_ID_RtAddr_i)) )
+        ? {1'b1, 1'b1, 1'b1} : {1'b0, 1'b0, 1'b0};
+
+// always@(*)
+// begin
     
-    // default
-    PC_Stall_o = 1'b0;
-    IF_ID_Stall_o = 1'b0;
-    stall_o = 1'b0;
+//     // default
+//     PC_Stall_o = 1'b0;
+//     IF_ID_Stall_o = 1'b0;
+//     stall_o = 1'b0;
 
-    if(ID_EX_MemRead_i && ((ID_EX_RtAddr_i == IF_ID_RsAddr_i) ||
-                         (ID_EX_RtAddr_i == IF_ID_RtAddr_i)) )
-    begin
-        PC_Stall_o = 1'b1;
-        IF_ID_Stall_o = 1'b1;
-        stall_o = 1'b1;
-    end
-
-end
+//     if(ID_EX_MemRead_i && ((ID_EX_RtAddr_i == IF_ID_RsAddr_i) ||
+//                          (ID_EX_RtAddr_i == IF_ID_RtAddr_i)) )
+//     begin
+//         PC_Stall_o = 1'b1;
+//         IF_ID_Stall_o = 1'b1;
+//         stall_o = 1'b1;
+//     end
+// 
+// end
 endmodule
